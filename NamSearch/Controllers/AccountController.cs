6c481312e2b04bb8a3e146dd8375e36efbe7e4dd
Scott Kirkland <srkirkland@gmail.com>
2010-12-20 16:54:14 -0800

    moved the auth to cas

--- NamSearch/Controllers/AccountController.cs
+++ NamSearch/Controllers/AccountController.cs
@@ -8,50 +8,51 @@ namespace NamSearch.Controllers
     [HandleTransactionsManually]
     public class AccountController : ApplicationController
     {
-        public ActionResult LogOn(string returnUrl)
-        {
-            return View();    
-        }
-
-        [HttpPost]
-        [BypassAntiForgeryToken]
-        public ActionResult LogOn(string returnUrl, string pass)
-        {
-            if (pass.ToLower() == "caes")
-            {
-                FormsAuthentication.SetAuthCookie("admin", false);
-                return RedirectToAction("Index", "Home");
-            }
-            
-            TempData["message"] = "Password incorrect";
-
-            return View();
-        }
-
-        public ActionResult LogOff()
-        {
-            FormsAuthentication.SignOut();
-            return RedirectToAction("LogOn");
-        }
         //public ActionResult LogOn(string returnUrl)
         //{
-        //    string resultUrl = CASHelper.Login(); //Do the CAS Login
+        //    return View();    
+        //}
 
-        //    if (resultUrl != null)
+        //[HttpPost]
+        //[BypassAntiForgeryToken]
+        //public ActionResult LogOn(string returnUrl, string pass)
+        //{
+        //    if (pass.ToLower() == "caes")
         //    {
-        //        return Redirect(resultUrl);
+        //        FormsAuthentication.SetAuthCookie("admin", false);
+        //        return RedirectToAction("Index", "Home");
         //    }
-
-        //    TempData["URL"] = returnUrl;
-
+            
+        //    TempData["message"] = "Password incorrect";
 
         //    return View();
         //}
 
-        //public ActionResult LogOut()
+        //public ActionResult LogOff()
         //{
         //    FormsAuthentication.SignOut();
-        //    return Redirect("https://cas.ucdavis.edu/cas/logout");
+        //    return RedirectToAction("LogOn");
         //}
+
+        public ActionResult LogOn(string returnUrl)
+        {
+            string resultUrl = CASHelper.Login(); //Do the CAS Login
+
+            if (resultUrl != null)
+            {
+                return Redirect(resultUrl);
+            }
+
+            TempData["URL"] = returnUrl;
+
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("https://cas.ucdavis.edu/cas/logout");
+        }
     }
 }
